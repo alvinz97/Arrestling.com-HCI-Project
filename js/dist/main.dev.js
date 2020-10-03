86354b380f8d77ebe6e28dd425a807dec3058170
alvinz97 <it18211306@my.sliit.lk>
2020-10-04 00:23:33 +0530

    Complete register

--- js/dist/main.dev.js
+++ js/dist/main.dev.js
@@ -317,5 +317,81 @@ jQuery(document).ready(function ($) {
       }
     });
     return false;
+  }); // REGISTER AREA 
+
+  $('#register-form').submit(function () {
+    var f = $(this).find('.form-group'),
+        ferror = false,
+        emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
+    f.children('input').each(function () {
+      // run all inputs
+      var i = $(this); // current input
+
+      var rule = i.attr('data-rule');
+
+      if (rule !== undefined) {
+        var ierror = false; // error flag for current input
+
+        var pos = rule.indexOf(':', 0);
+
+        if (pos >= 0) {
+          var exp = rule.substr(pos + 1, rule.length);
+          rule = rule.substr(0, pos);
+        } else {
+          rule = rule.substr(pos + 1, rule.length);
+        }
+
+        switch (rule) {
+          case 'required':
+            if (i.val() === '') {
+              ferror = ierror = true;
+            }
+
+            break;
+
+          case 'email':
+            if (!emailExp.test(i.val())) {
+              ferror = ierror = true;
+            }
+
+            break;
+
+          case 'minlen':
+            if (i.val().length < parseInt(exp)) {
+              ferror = ierror = true;
+            }
+
+            break;
+        }
+
+        i.next('.validation').html(ierror ? i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input' : '').show('blind');
+      }
+    });
+    if (ferror) return false;else var str = $(this).serialize();
+    var action = $(this).attr('action');
+
+    if (!action) {
+      action = 'includes/register.inc.php';
+    }
+
+    var email = $('#email').val();
+    var password = $('#password').val();
+    $.ajax({
+      type: "POST",
+      url: action,
+      data: {
+        email: email,
+        password: password,
+        status: 'login'
+      },
+      success: function success(data) {
+        if (data == 'OK') {
+          alert('Successfull');
+        } else if (data == 'Error') {
+          alert('Error Occur');
+        }
+      }
+    });
+    return false;
   });
 });
