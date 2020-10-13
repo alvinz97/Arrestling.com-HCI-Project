1249d33153fe0ac90896e38d3f95fa9204e92521
it18211306@my.sliit.lk <it18211306@my.sliit.lk>
2020-10-14 00:17:22 +0530

    Create a modal for remove btn

--- js/main.js
+++ js/main.js
@@ -17,7 +17,6 @@ AOS.init({
     delay: 50,
 });
 
-
 $("#ad-section .close-btn").click(function () {
     $("#ad-section img").hide();
     $("#ad-section .close-btn").hide();
@@ -425,33 +424,9 @@ window.onclick = function(event) {
     }
 }
 
-
-// Remove confirmation
-$("#confirmModal").hide();
-$("#confirmModal").modal({
- fadeDuration: 1000,
+$("#confirmModalLink").click(function () {
+    $("#confirmModal").modal({
+        fadeDuration: 1000,
+        fadeDelay: 1.5
+    });
 });
-
-
-
-// Single project
-
-// $(document).ready(function() {
-// 	$('.popup-gallery').magnificPopup({
-// 		delegate: 'a',
-// 		type: 'image',
-// 		tLoading: 'Loading image #%curr%...',
-// 		mainClass: 'mfp-img-mobile',
-// 		gallery: {
-// 			enabled: true,
-// 			navigateByImgClick: true,
-// 			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-// 		},
-// 		image: {
-// 			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-// 			titleSrc: function(item) {
-// 				return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
-// 			}
-// 		}
-// 	});
-// });
