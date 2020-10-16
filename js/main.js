901cbec7c1abcb1c587e5229b70783e24d7b9cfd
gavidya <it18225716@my.sliit.lk>
2020-10-16 18:43:22 +0530

    Create forgot-password page and completed

--- js/main.js
+++ js/main.js
@@ -405,6 +405,60 @@ jQuery(document).ready(function ($) {
         return false;
     });
 
+    // FORGOT PASSWORD AREA
+    $('#forgot-password-form').submit(function () {
+        var f = $(this).find('.form-group'),
+            ferror = false,
+            emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
+
+        f.children('input').each(function () { // run all inputs
+
+            var i = $(this); // current input
+            var rule = i.attr('data-rule');
+
+            if (rule !== undefined) {
+                var ierror = false; // error flag for current input
+                var pos = rule.indexOf(':', 0);
+                if (pos >= 0) {
+                    var exp = rule.substr(pos + 1, rule.length);
+                    rule = rule.substr(0, pos);
+                } else {
+                    rule = rule.substr(pos + 1, rule.length);
+                }
+
+                switch (rule) {
+                    case 'email':
+                        if (!emailExp.test(i.val())) {
+                            ferror = ierror = true;
+                        }
+                        break;
+                }
+                i.next('.validation').html((ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
+            }
+        });
+
+        if (ferror) return false;
+        else var str = $(this).serialize();
+        var action = $(this).attr('action');
+        if (!action) {
+            action = 'includes/forgot-password.inc.php';
+        }
+
+        var email = $('#email').val();
+
+        $.ajax({
+            type: "POST",
+            url: action,
+            data: {
+                email: email,
+                status: 'forgot-password'
+            },
+            success: function (data) {
+
+            }
+        });
+        return false;
+    });
 
     // TRACKING AREA
     $('#contact-form').submit(function () {
